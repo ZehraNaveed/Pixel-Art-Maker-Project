9167c7c835171794a6a14485d61b21a687e6976d
ZehraNaveed <84715502+ZehraNaveed@users.noreply.github.com>
2021-06-01 22:47:55 +0100

    2nd Commit fixes to grid height

--- designs.js
+++ designs.js
@@ -1,4 +1,6 @@
+// Select color input
 const colorPick = document.getElementById("colorPick");
+// Select size input
 const sizePicker = document.getElementById("sizePicker");
 const boxHeight = document.getElementById("inputHeight");
 const boxWidth = document.getElementById("inputWidth");
@@ -9,20 +11,23 @@ function formSubmit(event) {
     canvasBoard.innerHTML = "";
     makeGrid(boxHeight, boxWidth);
   };
+// sizePicker function
 sizePicker.addEventListener("click", function(event){
-  formSubmit(event);
-  makeGrid(boxHeight, boxWidth);
-        })
-  function makeGrid(rows, cols){
-      for (let r = 0; r < rows.value; r++) {
-          let row = canvasBoard.insertRow(0);
-          console.log(row)
-          for (let c = 0; c < cols.value; c++) {
-              let cell = row.insertCell(0);
-              console.log(cell)
-              cell.addEventListener("click", e => {
-                  e.target.style.background = colorPicker.value;
-             });
-          };
-        };
-      };
+    formSubmit(event);
+    makeGrid(boxHeight, boxWidth);
+        });
+// when size is selected by user, call makeGrid()
+function makeGrid(rows, cols){
+    canvasBoard.innerHTML = '';
+    for (let r = 0; r < rows.value; r++) {
+        let row = canvasBoard.insertRow(0);
+        console.log(row)
+        for (let c = 0; c < cols.value; c++) {
+            let cell = row.insertCell(0);
+            console.log(cell)
+            cell.addEventListener("click", e => {
+              e.target.style.background = colorPicker.value;
+         });
+       };
+    };
+  };
